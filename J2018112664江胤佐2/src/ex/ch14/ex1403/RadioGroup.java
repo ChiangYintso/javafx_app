b53c5c7959e8d533957142ef091fa818effef72e
ChiangYintso <392711804@qq.com>
2019-11-14 21:55:45 +0800

    add: ex1403

---
+++ J2018112664江胤佐2/src/ex/ch14/ex1403/RadioGroup.java
@@ -0,0 +1,16 @@
+package ex.ch14.ex1403;
+
+import javafx.application.Application;
+import javafx.scene.control.RadioButton;
+import javafx.stage.Stage;
+
+public class RadioGroup extends Application {
+
+	@Override
+	public void start(Stage arg0) throws Exception {
+		// TODO Auto-generated method stub
+		final RadioButton rb1 = new RadioButton("ºìÉ«");
+		final RadioButton rb2 = new RadioButton("ÂÌÉ«");
+	}
+
+}
