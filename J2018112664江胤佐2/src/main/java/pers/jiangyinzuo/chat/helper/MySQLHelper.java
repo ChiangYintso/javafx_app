55b3298b54add219cbe9ed37e463dc72ea4fe37f
ChiangYintso <392711804@qq.com>
2019-11-23 21:07:29 +0800

    init console application

--- J2018112664江胤佐2/src/main/java/pers/jiangyinzuo/chat/helper/MySQLHelper.java
+++ J2018112664江胤佐2/src/main/java/pers/jiangyinzuo/chat/helper/MySQLHelper.java
@@ -1,8 +1,15 @@
 package main.java.pers.jiangyinzuo.chat.helper;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.Statement;
 
 public class MySQLHelper {
+	private static final String username = "root";
+	private static final String password = "ABCabc123!@#";
+	private static final String url = "jdbc:mysql://localhost:3306/hellojyz?serverTimezone=GMT%2B8";
+	private static PreparedStatement preparedStatement;
+	private static Statement statement;
 	private static String driver;
 	private static Connection conn;
 
---
+++ J2018112664江胤佐2/src/main/java/pers/jiangyinzuo/chat/ui/console/Main.java
@@ -0,0 +1,11 @@
+package main.java.pers.jiangyinzuo.chat.ui.console;
+
+/**
+ * 网络聊天室控制台UI的入口类
+ * @author Jiang Yinzuo
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
