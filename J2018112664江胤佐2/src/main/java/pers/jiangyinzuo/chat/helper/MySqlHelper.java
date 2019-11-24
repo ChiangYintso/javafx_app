2ad80ac915197e9e971ed2c9260c892ed2582451
ChiangYintso <392711804@qq.com>
2019-11-24 11:05:11 +0800

    init console application

---
+++ J2018112664江胤佐2/src/main/java/pers/jiangyinzuo/chat/helper/MySqlHelper.java
@@ -0,0 +1,75 @@
+package main.java.pers.jiangyinzuo.chat.helper;
+
+import java.sql.*;
+
+/**
+ * 连接Mysql的辅助类
+ * @author Jiang Yinzuo
+ */
+public class MySqlHelper {
+	private static final String username = "root";
+	private static final String password = "ABCabc123!@#";
+	private static final String url = "jdbc:mysql://localhost:3306/hellojyz?serverTimezone=GMT%2B8";
+
+	private static PreparedStatement preparedStatement;
+	private static Statement statement;
+	private static String driver;
+	private static Connection conn;
+
+	{
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		getConnection();
+	}
+
+	/**
+	 * 单例模式获取连接
+	 * @return
+	 */
+	private static Connection getConnection() {
+		try {
+			if (conn == null || conn.isClosed()) {
+				conn = DriverManager.getConnection(url, username, password);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return conn;
+	}
+
+	public static ResultSet executeQuery(String sql, Object[] parameters) {
+		loadPreparedStatement(sql, parameters);
+		ResultSet resultSet = null;
+		try {
+			resultSet = preparedStatement.executeQuery();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return resultSet;
+	}
+
+	public static int executeUpdate(String sql, Object[] parameters) {
+		loadPreparedStatement(sql, parameters);
+		int rowCount = -1;
+		try {
+			rowCount = preparedStatement.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return rowCount;
+	}
+
+	private static void loadPreparedStatement(String sql, Object[] parameters) {
+		try {
+			preparedStatement = conn.prepareStatement(sql);
+			for (int i = 1; i <= parameters.length; ++i) {
+				preparedStatement.setObject(i, parameters[i]);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+}
